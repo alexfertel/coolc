0d9895d5e48d8b25a92c29faa6a4c9b3784e551a
Alexander Gonzalez <alexfertel97@gmail.com>
2018-11-22 13:21:41 -0500

    Check feature name duplication

--- src/cooltypes.py
+++ src/cooltypes.py
@@ -8,6 +8,11 @@ import visitor as visitor
 class TypesVisitor:
     def __init__(self):
         self.types = {}
+        self.current_class = {
+            'name': '',
+            'method_names': set(),
+            'attr_names': set()
+        }
 
     @visitor.on('node')
     def visit(self, node):
@@ -22,9 +27,26 @@ class TypesVisitor:
 
     @visitor.when(ast.Class)
     def visit(self, node: ast.Class):
+        # Check if class is defined only once
         if node.name in self.types.keys():
             # or node.name in reserved.keys(): I think this gets fixed in lexing.
             return 0
 
+        self.current_class['name'] = self.node.name.value  # Visiting this class's features
+
+        # Check if features of this class are defined only once
+        unique = 1
+        for feature in node.features:
+            unique &= self.visit(feature)
+
+        # Add this classname to the defined types
         self.types[node.name.value] = node
-        return 1
+        return unique
+
+    @visitor.when(ast.ClassMethod)
+    def visit(self, node: ast.ClassMethod):
+        return 0 if node.name in self.current_class['method_names'] else 1
+
+    @visitor.when(ast.ClassAttribute)
+    def visit(self, node: ast.ClassAttribute):
+        return 0 if node.name in self.current_class['attr_names'] else 1
